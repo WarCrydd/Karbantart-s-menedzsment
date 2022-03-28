ba441ab92f0eef842cd0b6462d4ff351ac432586
Ákos Varga <IS850J@student.uni-pannon.hu>
2022-03-28 23:39:09 +0200

    abefore push

--- Szerver/out/build/x64-debug/Server/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Projektek/Karbantart-s-menedzsment/Szerver/Server/src
-# Build directory: C:/Projektek/Karbantart-s-menedzsment/Szerver/out/build/x64-debug/Server/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
