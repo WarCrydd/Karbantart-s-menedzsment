02d6cce43f6427cf7b3c4c4d8d86958dc4567f70
Akos <vargaakos2000@gmail.com>
2022-03-28 19:49:56 +0200

    server v2

--- Szerver/Server 2/Server 2/Program.cs
+++ Szerver/Server 2/Server 2/Program.cs
@@ -3,27 +3,13 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-
-// State object for reading client data asynchronously  
-public class StateObject
-{
-    // Size of receive buffer.  
-    public const int BufferSize = 1024;
-
-    // Receive buffer.  
-    public byte[] buffer = new byte[BufferSize];
-
-    // Received data string.
-    public StringBuilder sb = new StringBuilder();
-
-    // Client socket.
-    public Socket workSocket = null;
-}
+using Server_2;
 
 public class AsynchronousSocketListener
 {
-    // Thread signal.  
     public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+    static List<Thread> threads = new List<Thread>();
 
     public AsynchronousSocketListener()
     {
@@ -31,19 +17,19 @@ public class AsynchronousSocketListener
 
     public static void StartListening()
     {
-        // Establish the local endpoint for the socket.  
-        // The DNS name of the computer  
-        // running the listener is "host.contoso.com".  
         IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-        IPAddress ipAddress = ipHostInfo.AddressList[0];
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
-        Console.WriteLine(ipAddress.ToString());
+        for(int i = 0; i< ipHostInfo.AddressList.Length; i++)
+        {
+            Console.WriteLine("[" + i + "]: " + ipHostInfo.AddressList[i].ToString());
+        }
 
-        // Create a TCP/IP socket.  
+        IPAddress ipAddress = ipHostInfo.AddressList[Convert.ToInt16(Console.ReadLine())];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8888);
+        Console.WriteLine(ipAddress.ToString());
+ 
         Socket listener = new Socket(ipAddress.AddressFamily,
             SocketType.Stream, ProtocolType.Tcp);
-
-        // Bind the socket to the local endpoint and listen for incoming connections.  
+ 
         try
         {
             listener.Bind(localEndPoint);
@@ -51,16 +37,13 @@ public class AsynchronousSocketListener
 
             while (true)
             {
-                // Set the event to nonsignaled state.  
                 allDone.Reset();
-
-                // Start an asynchronous socket to listen for connections.  
-                Console.WriteLine("Waiting for a connection...");
+ 
+                //Console.WriteLine("Waiting for a connection...");
                 listener.BeginAccept(
                     new AsyncCallback(AcceptCallback),
                     listener);
 
-                // Wait until a connection is made before continuing.  
                 allDone.WaitOne();
             }
 
@@ -77,88 +60,89 @@ public class AsynchronousSocketListener
 
     public static void AcceptCallback(IAsyncResult ar)
     {
-        // Signal the main thread to continue.  
         allDone.Set();
 
-        // Get the socket that handles the client request.  
         Socket listener = (Socket)ar.AsyncState;
         Socket handler = listener.EndAccept(ar);
 
-        // Create the state object.  
-        StateObject state = new StateObject();
+        Sassion state = new Sassion();
         state.workSocket = handler;
-        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+        while(state.workSocket.Connected)
+        {
+            state.allDone.Reset();
+            handler.BeginReceive(state.buffer, 0, Sassion.BufferSize, 0,
             new AsyncCallback(ReadCallback), state);
+            state.allDone.WaitOne();
+        }
+        Sassion.sassionStopd(state);
+        //threads.Add(new Thread(state.start));
     }
 
     public static void ReadCallback(IAsyncResult ar)
     {
-        String content = String.Empty;
-
-        // Retrieve the state object and the handler socket  
-        // from the asynchronous state object.  
-        StateObject state = (StateObject)ar.AsyncState;
-        Socket handler = state.workSocket;
-
-        // Read data from the client socket.
-        int bytesRead = handler.EndReceive(ar);
-
-        if (bytesRead > 0)
+        try
         {
-            // There  might be more data, so store the data received so far.  
-            state.sb.Append(Encoding.ASCII.GetString(
-                state.buffer, 0, bytesRead));
+            String content = String.Empty;
 
-            // Check for end-of-file tag. If it is not there, read
-            // more data.  
-            content = state.sb.ToString();
-            if (content.IndexOf("<EOF>") > -1)
+            Sassion state = (Sassion)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
             {
-                // All the data has been read from the
-                // client. Display it on the console.  
-                Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
-                    content.Length, content);
-                // Echo the data back to the client.  
-                Send(handler, content);
+                state.sb.Append(Encoding.UTF8.GetString(
+                    state.buffer, 0, bytesRead));
+
+                content = state.sb.ToString();
+
+                state.write("Read " + content.Length +" bytes from client: " + content);
             }
-            else
-            {
-                // Not all data received. Get more.  
-                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReadCallback), state);
-            }
+
+            Send(state, content.Substring(0, 2) + state.solve(content));
+        }
+        catch (Exception ex)
+        {
+            Sassion state = (Sassion)ar.AsyncState;
+            if(!state.workSocket.Connected)
+                Console.WriteLine(ex.ToString());
         }
     }
 
-    private static void Send(Socket handler, String data)
+    private static void Send(Sassion state, String data)
     {
-        // Convert the string data to byte data using ASCII encoding.  
-        byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-        // Begin sending the data to the remote device.  
-        handler.BeginSend(byteData, 0, byteData.Length, 0,
-            new AsyncCallback(SendCallback), handler);
+        try
+        {
+            byte[] byteData = Encoding.UTF8.GetBytes(data);
+            state.write("data to client:" + data);
+            state.workSocket.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), state);
+        }
+        catch(Exception ex)
+        {
+            if (!state.workSocket.Connected)
+                Console.WriteLine(ex.ToString());
+        }
     }
 
     private static void SendCallback(IAsyncResult ar)
     {
+        Sassion state = (Sassion)ar.AsyncState;
         try
-        {
-            // Retrieve the socket from the state object.  
-            Socket handler = (Socket)ar.AsyncState;
+        {  
+            int bytesSent = state.workSocket.EndSend(ar);
+            state.write("Sent " + bytesSent + " bytes to client.");
 
-            // Complete sending the data to the remote device.  
-            int bytesSent = handler.EndSend(ar);
-            Console.WriteLine("Sent {0} bytes to client.", bytesSent);
-
-            handler.Shutdown(SocketShutdown.Both);
-            handler.Close();
-
+            state.workSocket.Shutdown(SocketShutdown.Both);
+            state.workSocket.Close();
+            
         }
         catch (Exception e)
         {
-            Console.WriteLine(e.ToString());
+            if (!state.workSocket.Connected)
+                Console.WriteLine(e.ToString());
         }
+        state.allDone.Set();
     }
 
     public static int Main(String[] args)
